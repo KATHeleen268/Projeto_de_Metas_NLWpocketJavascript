d41f0c7f75038dde01ddb57bc6986b8b7755c36c
Katheleen Eduarda <katheleeneduarda123@gmail.com>
2024-09-13 01:00:29 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const { select, input } = require('@inquirier/prompts')
+const { select, input, checkbox } = require('@inquirier/prompts')
 
 let meta = {
     value: 'Tomar 3L de água por dia',
     checked: false,
 }
+
 let metas = [meta ]
 
 const cadastrarMeta = async () => {
@@ -15,13 +16,40 @@ const cadastrarMeta = async () => {
     }
 
     metas.push(
-        {value: meta, checked: false  })
+        {value: meta, checked: false  }
+    )
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e  Enter para finalizar essa etapa",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    if(respostas.length == 0 ){
+        console.log("Nenhuma meta selecionada.")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta)=> {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log('Meta(s) marcadas como concluída(s)')
+
 }
 
 
 const start = async () => {
-
-
     while(true){
         
         const opcao = await select ({
@@ -50,16 +78,13 @@ const start = async () => {
                 console.log(metas)
                 break
             case "listar":
-                console.log("vamos listar")   
+                await listarMetas()  
                 break 
             case "sair":
                 console.log("Até a próxima!")
-                return
-
-                
-
+                return                
         }
     }
 }
 
-start ()
+start ();
